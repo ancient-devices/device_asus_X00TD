36e0f6bba7dd551d8be1212a550a456926b5708f
N4veenNK <naveen4264nk@gmail.com>
2022-02-19 12:48:10 +0530

    X00TD: Welcome `Ancient`
    Signed-off-by: N4veenNK <naveen4264nk@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,15 @@
 #
 # Copyright (C) 2020 The LineageOS Project
 #
+# Copyright (C) 2021 The AncientOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/ancient_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
-
+    ancient_X00TD-user \
+    ancient_X00TD-userdebug \
+    ancient_X00TD-eng
--- ancient_X00TD.mk
+++ ancient_X00TD.mk
@@ -1,6 +1,8 @@
 #
 # Copyright (C) 2020 The LineageOS Project
 #
+# Copyright (C) 2021 The AncientOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -9,18 +11,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AncientOS stuff
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := ancient_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
 PRODUCT_MANUFACTURER := asus
+
+# Gapps
+ANCIENT_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+# Inherit some AncientOS stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+ANCIENT_OFFICIAL := true
+ANCIENT_WEEABO := true
+FORCE_LAUNCHER3 := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
